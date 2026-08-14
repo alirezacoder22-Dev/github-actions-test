5ffc1daa81644901d1b1f268672dd0ea53b1c567
alirezacoder22-Dev <alirezacoder22@gmail.com>
2026-08-14 17:16:35 +0330

    Create bot.py
    A bot

---
+++ src/bot.py
@@ -0,0 +1,31 @@
+import os
+
+from telegram import Update
+from telegram.ext import (
+    Application,
+    ContextTypes,
+    MessageHandler,
+    filters,
+)
+
+
+TOKEN = "8734182879:AAGd-9sO90VKTA9HCRZvzqjgzTHaLmPFUXo"
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message and update.message.text:
+        await update.message.reply_text(update.message.text)
+
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(
+    MessageHandler(
+        filters.TEXT & ~filters.COMMAND,
+        echo
+    )
+)
+
+print("Bot is running...")
+
+app.run_polling()
